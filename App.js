68b3d02e2433a7584591aba4e8d19dff339ea3a6
Daniel Augusto Mattos <daniel.mattos@dotgroup.com.br>
2020-10-17 01:51:53 -0300

    create flip image

--- App.js
+++ App.js
@@ -1,114 +1,108 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {useState, useCallback, useRef, useMemo, useEffect} from 'react';
+import {Dimensions, Animated} from 'react-native';
+import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/dist/Feather';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const {width} = Dimensions.get('screen');
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Container = styled.View`
+  flex: 1;
+  height: 100%;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  padding: 16px;
+`;
 
-const App: () => React$Node = () => {
+const RowContainer = styled.View`
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const IconBase = styled(Icon).attrs({size: 50, color: 'black'})``;
+
+const IconTop = styled(IconBase).attrs({name: 'arrow-up'})`
+  margin-bottom: 24px;
+`;
+
+const IconBottom = styled(IconBase).attrs({
+  name: 'arrow-down',
+})`
+  margin-top: 24px;
+`;
+
+const Image = styled(Animated.Image)`
+  width: ${width * 0.5}px;
+  height: ${width * 0.5}px;
+`;
+
+const Input = styled.TextInput`
+  elevation: 2;
+  position: absolute;
+  top: 8px;
+  left: 8px;
+  right: 8px;
+  padding: 8px;
+  font-size: 18px;
+`;
+
+const App = () => {
+  const scaleX = useRef(new Animated.Value(1)).current;
+  const scaleY = useRef(new Animated.Value(1)).current;
+  const [imageUrl, setImageUrl] = useState(
+    'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2020%2F06%2F26%2Forange-kitten-955480082-2000.jpg',
+  );
+
+  scaleX.addListener((value) => console.log('value', value));
+  scaleY.addListener((value) => console.log('value', value));
+
+  const setScale = (position) => {
+    if (position === 'x') {
+      Animated.timing(scaleX, {
+        toValue: scaleX._value === 1 ? -1 : 1,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+      return;
+    }
+    Animated.timing(scaleY, {
+      toValue: scaleY._value === 1 ? -1 : 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <Container>
+      <Input
+        placeholder="Insert a image url here..."
+        onChangeText={(text) => text && setImageUrl(text)}
+      />
+      <IconTop onPress={() => setScale('y')} />
+      <RowContainer>
+        <IconBase name="arrow-left" onPress={() => setScale('x')} />
+        <Image
+          style={{
+            transform: [
+              {
+                scaleX,
+              },
+              {
+                scaleY,
+              },
+            ],
+          }}
+          source={{
+            uri: imageUrl,
+          }}
+        />
+        <IconBase name="arrow-right" onPress={() => setScale('x')} />
+      </RowContainer>
+      <IconBottom onPress={() => setScale('y')} />
+    </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
